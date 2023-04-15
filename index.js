0bfc6541e68e889a4115bfb5023ac505536c87fb
tarikul-islam <tariikul12@gmail.com>
2023-04-15 11:14:06 +0600

    my first concept of server

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const phones = require('./phones.json')
+const app = express();
+const port = 5000;
+
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send('my phone information coming soon moon')
+})
+app.get('/phones',(req,res)=>{
+    res.send(phones);
+})
+app.get('/phones/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    console.log('i need data for id:', id)
+    const phone = phones.find(phone =>phone.id ===id)
+    res.send(phone);
+})
+app.listen(port,()=>{
+    console.log(`My phone server is running on port ${port}`)
+})
